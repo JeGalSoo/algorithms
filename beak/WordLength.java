f28f60b444219be89a9735d4544612cd6d88a170
JeGalSoo <jgs0318@gmail.com>
2024-04-07 15:47:55 +0900

    240407

---
+++ beak/WordLength.java
@@ -0,0 +1,10 @@
+package beak;
+
+import java.util.Scanner;
+
+public class WordLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next().length());
+    }
+}
