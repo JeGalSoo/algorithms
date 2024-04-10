1568769a90968675ca78cc72f01d889ff0a7945c
JeGalSoo <jgs0318@gmail.com>
2024-04-10 23:01:52 +0900

    240410

--- Main.java
+++ Main.java
@@ -1,36 +1,12 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        Scanner scanner = new Scanner(System.in);
-        System.out.printf("ID?");
-        String id = scanner.next();
-        System.out.printf("비번?");
-        String password = scanner.next();
-        System.out.printf("비번확인?");
-        String confirm = scanner.next();
-        System.out.printf("이름?");
-        String name = scanner.next();
-        System.out.printf("주민번호?");
-        String ssn = scanner.next();
-        System.out.printf("전화번호?");
-        String phone = scanner.next();
-        System.out.printf("주소?");
-        String address = scanner.next();
-        System.out.printf("직업?");
-        String job = scanner.next();
-        System.out.println("회원가입");
-        System.out.println("ID : " + id);
-        System.out.println("비번 : " + password);
-        System.out.println("비번확인 : " + confirm);
-        System.out.println("이름 : " + name);
-        System.out.println("주민번호 : " + ssn);
-        System.out.println("전화번호 : " + phone);
-        System.out.println("주소 : " + address);
-        System.out.println("직업 : " + job);
+        Scanner sc = new Scanner(System.in);
+        int total = 0;
+        for(int i=0; i<5; i++){
+            total += sc.nextInt();
+        }
+        System.out.println(total);
     }
 }
--- beak/SafariWorld.java
+++ beak/SafariWorld.java
@@ -2,6 +2,8 @@ package beak;
 
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class SafariWorld {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
