76564d4914d3cb468b6217be8580b7f24a7b7292
JeGalSoo <jgs0318@gmail.com>
2024-04-13 19:57:26 +0900

    240413

---
+++ beak/Welcome.java
@@ -0,0 +1,9 @@
+package beak;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
