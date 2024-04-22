cdfa88461b7567b7dd06ddb07cc3a00a7bc2096f
JeGalSoo <jgs0318@gmail.com>
2024-04-22 19:18:49 +0900

    240422 algorithm

--- beak/R2.java
+++ beak/R2.java
@@ -1,6 +1,8 @@
+package beak;
+
 import java.util.Scanner;
 
-public class Main {
+public class R2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(-sc.nextInt()+sc.nextInt()*2);
