1bb6dfc172bfec63bd6ab7ef6cfec3a074725025
JeGalSoo <jgs0318@gmail.com>
2024-04-28 16:32:04 +0900

    240428 algorithm

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(-sc.nextInt()+sc.nextInt()*2);
+        int a = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
+        int d = (int) Math.ceil(b /sc.nextDouble());
+        int e = (int) Math.ceil(c /sc.nextDouble());
+        System.out.println((d>e)?a-d:a-e);
     }
 }
