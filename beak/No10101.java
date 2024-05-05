5a1f559c85b9394ee12699cea4b4f9657bb5c3ed
JeGalSoo <jgs0318@gmail.com>
2024-05-05 21:39:25 +0900

    240505 pratice algorithm

--- beak/No10101.java
+++ beak/No10101.java
@@ -1,20 +1,22 @@
+package beak;
+
 import java.util.Scanner;
 
-public class Main {
+public class No10101 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        if(a+b+c == 180){
-            if(a==b&&a==c){
+        if (a + b + c == 180) {
+            if (a == b && a == c) {
                 System.out.printf("Equilateral");
-            } else if (a==b||a==c||b==c) {
+            } else if (a == b || a == c || b == c) {
                 System.out.printf("Isosceles");
-            }else {
+            } else {
                 System.out.printf("Scalene");
             }
-        }else {
+        } else {
             System.out.println("Error");
         }
     }
---
+++ beak/No5717.java
@@ -0,0 +1,15 @@
+package beak;
+
+import java.util.Scanner;
+
+public class No5717 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a==0)return;
+            System.out.println(a+b);
+        }
+    }
+}
