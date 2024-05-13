a3ecc4a9ae8810aff70569a2dff2380ab937f319
JeGalSoo <jgs0318@gmail.com>
2024-05-13 09:37:40 +0900

    240513 practice algorithm

--- Main.java
+++ Main.java
@@ -3,20 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        int min1 = 100;
-        int min2 = 100;
-        for(int i=0; i<4; i++){
-            int a = sc.nextInt();
-            sum+=a;
-            min1 = Math.min(min1, a);
+        int a = sc.nextInt();
+        for (int i=0; i<a; i++){
+            int b = sc.nextInt();
+            for (int j=0; j<b; j++){
+                System.out.print("=");
+            }
+            System.out.println();
         }
-        for(int i=0; i<2; i++){
-            int a = sc.nextInt();
-            sum+=a;
-            min2 = Math.min(min2, a);
-        }
-        sum-=(min1+min2);
-        System.out.println(sum);
     }
 }
---
+++ beak/No13752.java
@@ -0,0 +1,24 @@
+package beak;
+
+import java.util.Scanner;
+
+public class No13752 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int min1 = 100;
+        int min2 = 100;
+        for(int i=0; i<4; i++){
+            int a = sc.nextInt();
+            sum+=a;
+            min1 = Math.min(min1, a);
+        }
+        for(int i=0; i<2; i++){
+            int a = sc.nextInt();
+            sum+=a;
+            min2 = Math.min(min2, a);
+        }
+        sum-=(min1+min2);
+        System.out.println(sum);
+    }
+}
