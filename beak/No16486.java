1d6e9aefa75c709a9c6ac40fc9a61f185836aaac
JeGalSoo <jgs0318@gmail.com>
2024-05-21 09:51:51 +0900

    240521 practice algorithm

--- beak/No16486.java
+++ beak/No16486.java
@@ -1,6 +1,8 @@
+package beak;
+
 import java.util.Scanner;
 
-public class Main {
+public class No16486 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float a = sc.nextFloat();
