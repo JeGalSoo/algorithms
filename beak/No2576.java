3ef1d79a0831e9aed03dbe112569df4edefd8480
JeGalSoo <jgs0318@gmail.com>
2024-06-06 22:03:10 +0900

    240606 practice algorithm

--- beak/No2576.java
+++ beak/No2576.java
@@ -1,6 +1,8 @@
+package beak;
+
 import java.util.Scanner;
 
-public class Main {
+public class No2576 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum = 0;
