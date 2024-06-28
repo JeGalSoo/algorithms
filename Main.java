b474a967c2d29202df8ae95f9ef3b7745946106d
JeGalSoo <jgs0318@gmail.com>
2024-06-28 17:02:36 +0900

    240628 practice algorithm

--- Main.java
+++ Main.java
@@ -1,29 +1,26 @@
-import java.math.BigInteger;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = 64;
-        int count = 0;
-        int temp = 0;
-        int result = a;
-        while (true) {
-            if (a == 64) {
-                count++;
-                break;
-            }
-            b /= 2;
-            if (b <= a) {
-                temp += b;
-                count++;
-                if (result == temp) {
-                    break;
-                }
-                a -= b;
-            }
+        Random rand = new Random();
+        System.out.print("출생년도를 입력해 주세요.(yyyy):");
+        String year = sc.next();
+        year = year.substring(2);
+        System.out.print("출생월을 입력해 주세요.(mm):");
+        String month = sc.next();
+        if (month.length()<2)month="0"+month;
+        System.out.print("출생일을 입력해 주세요.(dd):");
+        String day = sc.next();
+        if (day.length()<2)day="0"+day;
+        System.out.print("성별을 입력해 주세요.(m/f):");
+        String sex = sc.next();
+        String random = String.format("%06d", (rand.nextInt(999999)+1));
+        if (sex.equals("m")) {
+            System.out.print(year + month + day + "-3" + random);
+        } else {
+            System.out.print(year + month + day + "-4" + random);
         }
-        System.out.println(count);
     }
 }
