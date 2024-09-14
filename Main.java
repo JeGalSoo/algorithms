fd60645e899fb20654423e8cbe99a6c7239387c3
JeGalSoo <jgs0318@gmail.com>
2024-09-14 20:05:42 +0900

    practice algorithm

--- Main.java
+++ Main.java
@@ -3,21 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int pt = 0;
 
-        String str = br.readLine();
-
-        int count = 0;
-        char word = str.charAt(0);
-
-        for (int i = 1; i < str.length(); i++) {
-            if (word != str.charAt(i)) {
-                word = str.charAt(i);
-                count++;
+        int base = 0;
+        while (base++ <= 30000) {
+            String tmp = String.valueOf(base);
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == s.charAt(pt))
+                    pt++;
+                if (pt == s.length()) {
+                    System.out.println(base);
+                    return;
+                }
             }
         }
-        System.out.println((count / 2) + count % 2);
     }
 }
