acafcb24218526ec48ace4b5a44dedc1400c0ba3
JeGalSoo <jgs0318@gmail.com>
2024-09-26 17:35:10 +0900

    practice algorithm

--- beak/No1448.java
+++ beak/No1448.java
@@ -1,10 +1,12 @@
+package beak;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class Main {
+public class No1448 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
