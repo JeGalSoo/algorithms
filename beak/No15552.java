f1f82e4f1ad1693689a10773cef28631abe06500
JeGalSoo <jgs0318@gmail.com>
2024-11-18 23:37:14 +0900

    practice algorithm

--- beak/No15552.java
+++ beak/No15552.java
@@ -1,7 +1,9 @@
+package beak;
+
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main{
+public class No15552 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
