1324958e2f9866e2ed2d8359f1b014cb7bb4dea9
unknown <jgs0318@gmail.com>
2025-03-17 00:59:15 +0900

    pratice algorithms

--- Main.java
+++ Main.java
@@ -2,28 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = 0;
-        int min = -1;
-        for(int i = a; i <= b; i++){
-            boolean chk = true;
-            if (i<2) {
-                continue;
-            }
-            for(int j=2; j <= Math.sqrt(i); j++){
-                if(i % j == 0){
-                    chk = false;
-                    break;
-                }
-            }
-            sum += chk ? i : 0;
-            min = chk && min == -1 ? i : min;
-        }
-        if(min != -1){
-            System.out.println(sum);
-        }
-        System.out.println(min);
+        System.out.println(1);
+        System.out.println(0);
     }
 }
---
+++ beak/No24265.py
@@ -0,0 +1,3 @@
+a = int(input())
+print(int((a-1)*a-(a-1)*a/2))
+print(2)
